a0dbeb9b6994c9c81b94ea3855ff9c5ee3f8168e
陈宏葆 <chenhongbao@outlook.com>
2020-09-17 20:32:51 +0800

    Add moving average-like indicators.

---
+++ src/main/com/nabiki/ta/EMA.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2020 Hongbao Chen <chenhongbao@outlook.com>
+ *
+ * Licensed under the  GNU Affero General Public License v3.0 and you may not use
+ * this file except in compliance with the  License. You may obtain a copy of the
+ * License at
+ *
+ *                    https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Permission is hereby  granted, free of charge, to any  person obtaining a copy
+ * of this software and associated  documentation files (the "Software"), to deal
+ * in the Software  without restriction, including without  limitation the rights
+ * to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+ * copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+ * IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+ * FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+ * AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.nabiki.ta;
+
+/**
+ * Exponential moving average indicator computed with the following equation:<br/>
+ * <code>
+ *   n-th day has EMA(n) while previous day has EMA(n-1). Given alpha ranging from
+ *   (0, 1) and  the n-th day input d,<br/>
+ *   EMA(n) = alpha x d + (1 - alpha) x EMA(n-1)
+ * </code>
+ */
+public class EMA extends MA {
+  private static final double ZERO_DAY_EMA = 0.0D;
+  private final double alpha;
+
+  /**
+   * EMA indicator with default {@code alpha = 2 / (window + 1)}.
+   *
+   * @param window window size, must be positive.
+   */
+  public EMA(int window) {
+    super(window);
+    alpha = defaultAlpha();
+  }
+
+  public EMA(double alpha, int window) {
+    super(window);
+    if (alpha <= 0 || alpha >= 1)
+      throw new ValueOutOfBoundException(String.format("%f not in (0, 1)", alpha));
+    this.alpha = alpha;
+  }
+
+  @Override
+  public boolean add(Double d) {
+    var prev = ZERO_DAY_EMA;
+    if (size() > 0)
+      prev = get(size() - 1);
+    var t = alpha * d + (1 - alpha) * prev;
+    return super.add(t);
+  }
+
+  private double defaultAlpha() {
+    return 2.0 / (getWindow() + 1);
+  }
+}
---
+++ src/main/com/nabiki/ta/WMA.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2020 Hongbao Chen <chenhongbao@outlook.com>
+ *
+ * Licensed under the  GNU Affero General Public License v3.0 and you may not use
+ * this file except in compliance with the  License. You may obtain a copy of the
+ * License at
+ *
+ *                    https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Permission is hereby  granted, free of charge, to any  person obtaining a copy
+ * of this software and associated  documentation files (the "Software"), to deal
+ * in the Software  without restriction, including without  limitation the rights
+ * to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+ * copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+ * IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+ * FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+ * AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.nabiki.ta;
+
+/**
+ * Weighted moving average indicator computed with the following equation:<br/>
+ * <code>
+ *   n-th day WMA(n) and n-th day input p(n). Given window w, the WMA(n) over the
+ *   latest window elements:<br/>
+ *   deno(n) = w x  p(n) + (w-1) x p(n-1) + ... + 2 x p(n-w+2) + p(n-w+1)<br/>
+ *   nume(n) = w + (w-1) + ... + 2 + 1 = n x (n+1) / 2<br/>
+ *   WMA(n) = deno(n) / nume(n)
+ * </code>
+ */
+public class WMA extends MA {
+  public WMA(int window) {
+    super(window);
+  }
+
+  @Override
+  public boolean add(Double d) {
+    base.add(d);
+    var n = Math.min(base.size(), getWindow());
+    var idx = base.size();
+    var t = 0.0D;
+    do {
+      t += n * base.get(--idx);
+    } while (--n > 0);
+    t /= n * (n + 1) / 2.0D;
+    return super.add(t);
+  }
+}
